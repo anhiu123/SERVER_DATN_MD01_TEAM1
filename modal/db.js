cba10cc78437f18ebf3d8097a1f659990675a2d4
admin <hieundph29367@fpt.edu.vn>
2024-03-20 15:11:13 +0700

    sửa db , list admin

--- modal/db.js
+++ modal/db.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://127.0.0.1:27017/DATN').catch((err)=>{
-    console.log(err);
-} );
+// mongoose.connect('mongodb://127.0.0.1:27017/DATN').catch((err)=>{
+    
+//     console.log(err);
+// } );
+
+// const connectDB = async () =>{
+//     try {
+//         await mongoose.connect(process.env.MONGOODB_CONNECT_URI)
+//         console.log("Connect SuccessFully");
+//     } catch (error) {
+//         console.log("connect Faild");
+//     }
+// }
+
+mongoose.connect("mongodb+srv://k53ka10e:k457GUmMnZgsoVzo@datn.6hbuotf.mongodb.net/DATN?retryWrites=true&w=majority&appName=DATN", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(error => console.error('Error connecting to MongoDB:', error));
+
 
     module.exports = {mongoose};
